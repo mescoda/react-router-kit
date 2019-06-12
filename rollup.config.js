b91ba055677a0c21ae969578897dceb3beb0a6c6
mescoda <mescodasun@gmail.com>
2019-06-12 17:22:46 +0800

    clear rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,56 +3,11 @@ import pkg from './package.json';
 
 import babel from 'rollup-plugin-babel';
 
+// suppress the external warning, exclude modules like react and react-router-dom
 const external = (id) => {
-    return !id.startsWith('.') && !id.startsWith('/');
+    return !(id.startsWith('.') || id.startsWith('/'));
 };
 
-const cjs = [
-    {
-        input: 'src/index.js',
-        output: {
-            file: `dist/cjs/${pkg.name}.js`,
-            format: 'cjs'
-        },
-        external,
-        plugins: [
-            babel({
-                exclude: '/node_modules/',
-                presets: [
-                    [
-                        '@babel/preset-env',
-                        {
-                            targets: {
-                                ie: 11
-                            },
-
-                            // disable BrowserslistConfig
-                            ignoreBrowserslistConfig: true,
-
-                            // Do not transform modules to CJS
-                            modules: false
-                        }
-                    ],
-                    '@babel/preset-react'
-                ],
-                plugins: [
-                    '@babel/plugin-proposal-object-rest-spread'
-                ]
-            })
-        ]
-    }
-];
-
-const esm = [
-    {
-        input: 'src/index.js',
-        output: {
-            file: `dist/esm/${pkg.name}.js`,
-            format: 'esm'
-        },
-        external
-    }
-];
 
 export default [
     {
